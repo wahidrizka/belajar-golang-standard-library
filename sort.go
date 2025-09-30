77dcfcd759f744c42ef473703e3c6fa419e638c6
wahidrizka <wahid.0310@students.amikom.ac.id>
2025-09-30 18:01:00 +0700

    add(sort): kode program tentang package sort

---
+++ sort.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+type UserSlice []User
+
+func (s UserSlice) Len() int {
+	return len(s)
+}
+
+func (s UserSlice) Less(i, j int) bool {
+	return s[i].Age < s[j].Age
+}
+
+func (s UserSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func main() {
+	users := []User{
+		{"Wahid", 23},
+		{"Rizka", 20},
+		{"Fathur", 25},
+		{"Rohman", 21},
+	}
+
+	sort.Sort(UserSlice(users))
+
+	fmt.Println(users)
+}
